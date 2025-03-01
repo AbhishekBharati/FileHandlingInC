723e4cf1fcca3be96ff2f21869b663c52291acb4
AbhishekBharati <210305105048@paruluniversity.ac.in>
2025-03-01 10:14:31 +0530

    Learnt how to read a file

--- fopen/read.c
+++ fopen/read.c
@@ -1,16 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
   FILE *file;
+  char data[100];
 
   file = fopen("test.txt", "r");
 
   if(file == NULL){
-    printf("We failed to open the file, Maybe the file doesn't exist");
-  } else {
-    printf("File opened Successfully..");
-    fclose(file);
+    printf("Failed to open the file. The file might not exist...");
   }
 
+  printf("Content of File : \n");
+
+  while(fgets(data, sizeof(data), file) != NULL){
+    printf("%s", data); // Line by line print krega.
+  }
+
+  printf("\n");
+  fclose(file);
+  file = fopen("test.txt", "r");
+
+  char word[50];
+  while(fscanf(file, "%s", word) != EOF){
+    printf("%s\n", word); // Yeh ek ek word ko read krega :-
+  }
+
+  printf("\n");
+  fclose(file);
+  file = fopen("test.txt", "r");
+  char ch;
+  while((ch = fgetc(file)) != EOF){
+    putchar(ch); // Character print krega.
+  }
+
+  fclose(file);
   return 0;
 }
